c2aca643b7339b7cfd2b9ada0b2867cd7af1587c
IvanChmil <52853189+IvanChmil@users.noreply.github.com>
2019-10-03 13:29:48 +0300

    Update GroupStudents.js

--- NODE JS HM/untitled/GroupStudents.js
+++ NODE JS HM/untitled/GroupStudents.js
@@ -3,16 +3,19 @@ const fs = require('fs');
 function timeStudent(group,student) {
 
     fs.mkdir(`./${group}`,err => {
+        
         if(!err){
             console.log("Group created")
         }
     });
 
     fs.mkdir(`./${group}/${student}`,err => {
+        
         if(!err){
             console.log("Student created")
         }
     })
 }
+
 
 module.exports= timeStudent;
